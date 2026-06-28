65b1fc5da98b358281db14848b0b340b21ecd241
Carl Joshua Asuliz <asulizc@gmail.com>
2026-06-28 14:13:03 +0800

    Feat: React routers setup

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './pages/home'
+import Projects from './pages/Projects'
+import Pricing from './pages/Pricing'
+import Community from './pages/Community'
+import { Routes } from 'react-router-dom'
+import Preview from './pages/Preview'
+import MyProjects from './pages/MyProjects'
+import View from './pages/View'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects/" element={<MyProjects />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/preview" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+         <Route path="/view/" element={<View />} />
+        
+          
+      </Routes>
+    </div>
+  )
+}
+
+export default App
